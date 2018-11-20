534bc1e513a39c1a7b405276c4ccbdead1037585
Cameron <cameronslatton@gmail.com>
2018-11-19 22:26:25 -0600

    Logic adds

--- Greetr.js
+++ Greetr.js
@@ -7,7 +7,72 @@
 
     var supportedLangs = ['en', 'es'];
     
-    Greetr.prototype = {};
+    Greetr.prototype = {
+        en: 'Hello',
+        es: 'Hola'
+    };
+
+    var formalGreetings = {
+        en: 'Greetings',
+        es: 'Saludos'
+    };
+
+    var logMessages = {
+        en:'Logged in',
+        es: 'Inició sesión'
+    };
+
+    Greetr.prototype = {
+
+        fullName: function() {
+            return this.fullName.firstName + ' ' + this.lastName;
+        },
+
+        validate: function() {
+            if (supportedLangs.indexOf(this.language) === -1){
+                throw "Invalid language";
+            }
+        },
+
+        greeting: function() {
+            return greetings[this.language] + ' ' + this.firstName + '!';
+        },
+
+        formalGreeting: function() {
+            return formalGreetings[this.language] + ', ' + this.fullName();
+        },
+
+        greet: function(formal) {
+            if (formal) {
+                msg = this.formalGreeting();
+            }
+            else {
+                msg = this.greeting();
+            }
+
+            if (console) {
+                console.log(msg);
+            }
+
+            // 'this' refers to the calling object at execution time
+            // makes the method chainable
+            return this;
+        },
+        
+        log: function() {
+            if (console) {
+                console.log(logMessages[this.language] + ': ' + this.fullName());
+            }
+            return this;
+        },
+
+        setLang: function(lang) {
+            this.language = lang;
+
+            this.validate();
+            return this;
+        }
+    };
 
     Greetr.init = function(
         firstName, 
